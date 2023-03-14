8a643701e6109fe375228597196c536ddd2ae7cc
DarshilkumarPatel <90249912+DarshilkumarPatel@users.noreply.github.com>
2023-03-13 22:52:32 -0400

    Client-in process

--- SpamDetectorClient/js/main.js
+++ SpamDetectorClient/js/main.js
@@ -1,4 +1,14 @@
-// TODO: onload function should retrieve the data needed to populate the UI
+const button1 = document.querySelector("#button1");
+const button2 = document.querySelector("#button2");
+const description1 = document.querySelector("#description1");
+const description2 = document.querySelector("#description2");
 
+button1.addEventListener("click", function() {
+  description1.classList.add("active");
+  description2.classList.remove("active");
+});
 
-
+button2.addEventListener("click", function() {
+  description2.classList.add("active");
+  description1.classList.remove("active");
+});
